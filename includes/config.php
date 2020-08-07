14c07bdf7ba36482b416c140ddff746389a3104a
Kushal Luitel <hipopgangster@gmail.com>
2020-08-07 08:05:23 +0545

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,16 @@
 <?php
 	switch ($_SERVER["SCRIPT_NAME"]) {
-		case "/php-template/about.php":
+		case "/about.php":
 			$CURRENT_PAGE = "About";
 			$PAGE_TITLE = "About Us";
 			break;
-		case "/php-template/contact.php":
+		case "/contact.php":
 			$CURRENT_PAGE = "Contact";
 			$PAGE_TITLE = "Contact Us";
 			break;
 		default:
 			$CURRENT_PAGE = "Index";
 			$PAGE_TITLE = "Muktinath Security";
-      $PAGE_TITLE_DESC = "Your security is our Responsibility";
+                        $PAGE_TITLE_DESC = "Your security is our Responsibility";
 	}
 ?>
